667f043bb0089244fb573ed1fb7eed2bb7ecd7aa
Vlad Chepaykyn <backdround@yandex.ru>
2023-03-05 20:53:54 +0300

    adjust colorscheme

--- lua/melting/colors.lua
+++ lua/melting/colors.lua
@@ -1,30 +1,31 @@
 local colors = {
   background = "#14171E",
-  foreground = "#C6C8D1",
-  gray = "#3D4252",
+  foreground = "#B4B7C5",
+  gray = "#333843",
+  black = "#1B1F27",
 
   dark = {
     gray = "#22252F",
-    black = "#1B1E25",
+    black = "#0B0C0E",
+    white = "#898EA4",
     red = "#BF5C80",
     green = "#6AA28C",
     yellow = "#C2BC85",
     blue = "#84A0C6",
-    magenta = "#A093C7",
+    magenta = "#8879B4",
     cyan = "#89B8C2",
-    white = "#C6C8D1",
   },
 
   bright = {
-    gray = "#6B7089",
-    black = "#1F222A",
+    gray = "#61677F",
+    black = "#1E2029",
+    white = "#E1E2E8",
     red = "#F2779B",
     green = "#88CEA2",
     yellow = "#DCDE92",
     blue = "#A9BBD5",
-    magenta = "#C6B9DB",
+    magenta = "#A093C7",
     cyan = "#9BE1D0",
-    white = "#E1E2E8",
   },
 }
 
