3b9322eeaff2efe75e72e522675ca71c7a33d8a7
Vlad Chepaykyn <backdround@yandex.ru>
2023-03-06 18:29:08 +0300

    format all

--- lua/lualine/themes/melting.lua
+++ lua/lualine/themes/melting.lua
@@ -2,39 +2,39 @@ package.loaded["melting.colors"] = nil
 local c = require("melting.colors")
 
 local colors = {
-	black = c.black,
-	white = c.foreground,
+  black = c.black,
+  white = c.foreground,
 
-	gray1 = c.gray1,
-	gray2 = c.gray2,
-	gray3 = c.gray3,
-	gray4 = c.gray4,
+  gray1 = c.gray1,
+  gray2 = c.gray2,
+  gray3 = c.gray3,
+  gray4 = c.gray4,
 
-	green = c.dark.green,
-	magenta = c.dark.magenta,
-	red = c.red,
-	blue = c.dark.blue,
+  green = c.dark.green,
+  magenta = c.dark.magenta,
+  red = c.red,
+  blue = c.dark.blue,
 }
 
 return {
-	normal = {
-		a = { bg = colors.gray3, fg = colors.black, gui = "bold" },
-		b = { bg = colors.gray2, fg = colors.white },
-		c = { bg = colors.gray1, fg = colors.gray4 },
-	},
-	insert = {
-		a = { bg = colors.green, fg = colors.black, gui = "bold" },
-	},
-	visual = {
-		a = { bg = colors.magenta, fg = colors.black, gui = "bold" },
-	},
-	replace = {
-		a = { bg = colors.red, fg = colors.black, gui = "bold" },
-	},
-	command = {
-		a = { bg = colors.blue, fg = colors.black, gui = "bold" },
-	},
-	inactive = {
-		a = { bg = colors.gray2, fg = colors.white, gui = "bold" },
-	},
+  normal = {
+    a = { bg = colors.gray3, fg = colors.black, gui = "bold" },
+    b = { bg = colors.gray2, fg = colors.white },
+    c = { bg = colors.gray1, fg = colors.gray4 },
+  },
+  insert = {
+    a = { bg = colors.green, fg = colors.black, gui = "bold" },
+  },
+  visual = {
+    a = { bg = colors.magenta, fg = colors.black, gui = "bold" },
+  },
+  replace = {
+    a = { bg = colors.red, fg = colors.black, gui = "bold" },
+  },
+  command = {
+    a = { bg = colors.blue, fg = colors.black, gui = "bold" },
+  },
+  inactive = {
+    a = { bg = colors.gray2, fg = colors.white, gui = "bold" },
+  },
 }
