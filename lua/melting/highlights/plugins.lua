e263526470066066ec5d4ef97827506f2f7b411c
Vlad Chepaykyn <backdround@yandex.ru>
2023-03-07 20:09:11 +0300

    add telescope support

--- lua/melting/highlights/plugins.lua
+++ lua/melting/highlights/plugins.lua
@@ -9,6 +9,25 @@ local plugins = {
   HopUnmatched = { fg = c.gray2, gui = "bold" },
   HopCursor = "Cursor",
   HopPreview = { fg = c.cyan, gui = "bold" },
+
+  -- Telescope
+  TelescopeTitle = "Title",
+  TelescopePromptNormal = { fg = c.white },
+  TelescopePromptCounter = { fg = c.magenta, gui = "bold" },
+
+  TelescopePromptPrefix = { fg = c.cyan, bg = c.gray1, gui = "bold" },
+  TelescopeSelectionCaret = "TelescopePromptPrefix",
+  TelescopeSelection = { fg = c.white, bg = c.gray1 },
+  TelescopeMatching = { fg = c.error, gui = "bold" },
+  TelescopeResultsNormal = { fg = c.gray4 },
+
+  TelescopePreviewNormal = "Normal",
+
+  TelescopeBorder = "FloatBorder",
+  TelescopePromptBorder = { fg = c.foreground },
+  TelescopePreviewBorder = "FloatBorder",
+  TelescopeResultsBorder = "FloatBorder",
+
 }
 
 return plugins
